549077678233993a72ed816be2e3690068d2129d
melikesoygullucu <melikesoygul@hotmail.com>
2024-03-27 14:04:57 +0300

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Melike Soygüllücü on 27.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
